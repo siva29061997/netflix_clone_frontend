a1946fe19a7db25e4cf2d048bb75ab24cb77a374
siva29061997 <siva29061997@gmail.com>
2022-12-23 09:25:09 +0530

    new

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,6 +1,6 @@
 export const env = {
     backendapi: "https://siva-netflix-clone.onrender.com/api",
     firebaseapi: "AIzaSyC3P_tRssNpUTmVTZpSxje4bHwiUOfHuQ4",
-    stripekey: "pk_test_51M1LmFAUlN9Z2Oc1wPSM5l9okLhUWjMbLPetnmOhYJGpjMFCadl7FL8CzE7b62Hg9MkZveWdtHCTL5jIgAA835JF00XKza7hWm"
+    stripekey: "pk_test_51MHqwBSE8r1Iffsgw6axaNLOfkEX822z9jeUQFuAcjVmGfeGl6xQhhwU86wXzn1quTa3hnXW3urmozdnTlYbsoTa00VuY0xORb"
     // # REACT_APP_FIREBASE_APIKEY : "AIzaSyAq7dF5EH5Jw96KQ5B - p0KQofmG - Xk_RsU"
 }
